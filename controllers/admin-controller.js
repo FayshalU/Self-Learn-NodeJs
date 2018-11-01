faff8bb61e7fa538b7d4c4f2925b284b4cf241c5
KhadijaKubra <39878211+KhadijaKubra@users.noreply.github.com>
2018-11-01 16:05:22 -0700

    Update admin-controller.js

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -30,21 +30,7 @@ router.get('/',function(req,res){
 
 });
 
-router.get('/addCourses',function(req,res){
 
-	userModel.getAdmin(req.session.un, function(result){
-		if(result.length > 0)
-		{
-      console.log(result[0].user_id);
-			res.render('admin/addCourses', {user:result[0]});
-		}
-		else
-		{
-			res.redirect('/login');
-		}
-	});
-
- });
 
 router.get('/addCourses',function(req,res){
 
@@ -71,7 +57,7 @@ router.post('/addCourses',function(req,res){
 	userModel.insertCourses(user,function(status){
 		if(status)
 		{
-			res.redirect('/admin');
+			res.redirect('/admin/showAvailableCourses');
 
 		}
 		else
